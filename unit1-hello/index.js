092c91f382238d6c7ced4a62bd8e0d069c77cae7
kentBui <duccuong.hpe@gmail.com>
2020-02-23 23:28:01 +0700

    add file learn pug engine and how to use it

--- unit1-hello/index.js
+++ unit1-hello/index.js
@@ -1,10 +1,26 @@
 var express = require('express');
-var app = express();// tra ve app
-var port = 3000; // define port 
-app.get('/users',(request, response)=>{
-	response.send('<h1>Hello world</h1>');// thực tế sẽ lấy dữ liệu từ database rồi render()
-});// tạo method get để lấy dữ liệu từ host có port = port với end point trong ''
-app.listen(port, ()=>{
-	console.log('Get data form port: '+port);
-}); // method để lắng nghe 
-// this is Hello world example
+var app = express();
+var port = 3000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', function(req,res){
+	res.render('index',{
+		name: 'kent'
+	});
+});
+
+app.get('/users', function(req,res){
+	res.render('users/index', {
+		users: [
+			{id: 1, name: 'kent1'},
+			{id: 2, name: 'kent2'},
+			{id: 3, name: 'kent3'}
+		]
+	});
+});
+
+app.listen(port, function(){
+	console.log('listen data from port:' + port);
+});
